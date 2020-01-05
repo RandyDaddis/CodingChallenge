64e626054f5f78cfed550948f74983955d717846
Randy Daddis <randy@daddis.tech>
2020-01-04 19:46:43 -0500

    refactor HTML element creation code block to new createElement() function re-order functions alphabetically

--- Content/js/site.js
+++ Content/js/site.js
@@ -4,42 +4,54 @@
               HOWEVER: Uglify fails when using ES6 let
    TODO: find suitable replacement package for Uglify */
 
+function addEvent(element, evnt, funct) {
 
-// add to cart
-function onAddtoCart() {
-
-    var itemCount = Number(document.querySelector('.shopping-cart-item-count').textContent);
-    itemCount++;
-    document.querySelector('.shopping-cart-item-count').textContent = itemCount.toString();
-
-    appendAddToCartPopup();
+    // kudos: https://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick
+    if (element.attachEvent)  // IE < 9
+        return element.attachEvent('on' + evnt, funct);
+    else
+        return element.addEventListener(evnt, funct, false);
 }
 
 function appendAddToCartPopup() {
 
-    var html =
-//'<aside id = "addToCartPopup" class="popup-overlay">' +
-'    <div class="popup-content">' +
-'        <div class="popup-header">Added to Cart</div>' +
-'        <div class="font-weight-600 p-3">Thank you</div>' +
-'        <div class="popup-close-button-container">' +
-'            <button id="removeAddtoCartPopup" onclick="removeAddtoCartPopup()" class="btn btn-dark-orange-gradient" data-toggle="button" aria-pressed="false">Close</button>' +
-'        </div>' +
-'    </div>'
- //'</aside >'
-    ;
-
-    // create popup
-    var element = document.createElement('aside');
-    element.setAttribute('id', 'addToCartPopup');
-    element.classList.add('popup-overlay');
-    element.innerHTML = html;
+    // create element
+    var element = createElement();
     // add to parent
     var parentNode = document.querySelector('.item-sidebar-right');
     parentNode.appendChild(element);
     // add click event listener
     var closeButton = document.getElementById('removeAddtoCartPopup');
     addEvent(closeButton, 'click', removeAddtoCartPopup);
+}
+
+function createElement() {
+    var html =
+        //'<aside id = "addToCartPopup" class="popup-overlay">' +
+        '    <div class="popup-content">' +
+        '        <div class="popup-header">Added to Cart</div>' +
+        '        <div class="font-weight-600 p-3">Thank you</div>' +
+        '        <div class="popup-close-button-container">' +
+        '            <button id="removeAddtoCartPopup" onclick="removeAddtoCartPopup()" class="btn btn-dark-orange-gradient" data-toggle="button" aria-pressed="false">Close</button>' +
+        '        </div>' +
+        '    </div>';
+    // create popup
+    var element = document.createElement('aside');
+    element.setAttribute('id', 'addToCartPopup');
+    element.classList.add('popup-overlay');
+    element.innerHTML = html;
+    return element;
+}
+
+// add to cart
+function onAddtoCart() {
+
+    // increment total items counter
+    var totalItemsCount = Number(document.querySelector('.shopping-cart-item-count').textContent);
+    totalItemsCount++;
+    document.querySelector('.shopping-cart-item-count').textContent = totalItemsCount.toString();
+    // display confirmation popup
+    appendAddToCartPopup();
 }
 
 function removeAddtoCartPopup() {
@@ -61,13 +73,4 @@ function removeAddtoCartPopup() {
             }
         });
     }
-}
-
-function addEvent(element, evnt, funct) {
-
-    // kudos: https://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick
-    if (element.attachEvent)  // IE < 9
-        return element.attachEvent('on' + evnt, funct);
-    else
-        return element.addEventListener(evnt, funct, false);
 }
